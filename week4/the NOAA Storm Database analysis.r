60d4707c113be5c2f504a9c85a25f6522fecf8ba
freefrog1986 <baihu1111@126.com>
2017-03-02 00:53:27 +0800

    finish code

--- week4/the NOAA Storm Database analysis.r
+++ week4/the NOAA Storm Database analysis.r
@@ -44,20 +44,34 @@ barplot(Top10Data$sum,density = 20,names.arg=Top10Data$EVTYPE)
 
 ## Greatest economic consequences events
 #### processing data
-TableStormData <- tbl_df(stormData)
-SumStormData <- 
-        select(TableStormData, EVTYPE, FATALITIES, INJURIES) %>%
-        group_by(EVTYPE) %>% 
-        summarize_all(.funs=sum)
+setnum<-function(a){
+        if((a=='K')|(a=='k')){a<-1000}
+        else if((a=='M')|(a=='m')){a<-1000000}
+        else if((a=='B')|(a=='b')){a<-1000000000}
+        else if((a=='H')|(a=='h')){a<-100}
+        else{a<-1}
+}
 
+multiply<-function(x,y){
+        x*y
+}
 
-SumStormData <- 
-        select(TableStormData, EVTYPE, FATALITIES, INJURIES) %>%
-        group_by(EVTYPE) %>% 
-        summarize(sum=sum(FATALITIES, INJURIES)) %>%
+DmgStormData <- select(TableStormData, 
+                       EVTYPE, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP)
+m<-lapply(DmgStormData$PROPDMGEXP,FUN = setnum)
+DmgStormData$PROPDMGEXP<-unlist(m)
+m<-lapply(DmgStormData$CROPDMGEXP,FUN = setnum)
+DmgStormData$CROPDMGEXP<-unlist(m)
+
+CostStormData <- 
+        mutate(DmgStormData, PROPs=multiply(PROPDMG, PROPDMGEXP)) %>%
+        mutate(CROPs=multiply(CROPDMG, CROPDMGEXP)) %>%
+        select(EVTYPE,PROPs,CROPs) %>%
+        group_by(EVTYPE) %>%
+        summarize(sum=sum(PROPs,CROPs)) %>%
         arrange(desc(sum))
 
-Top10Data<-SumStormData[1:10,]
+Top10Cost<-CostStormData[1:10,]
 
 #### plot the data
 barplot(Top10Data$sum,density = 20,names.arg=Top10Data$EVTYPE)
